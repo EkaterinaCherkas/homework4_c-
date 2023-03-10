8a6732462be8b2e35910b5e5e795b6768bc929f2
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-10 21:20:12 +0300

    add task 2 about принимает число и выдает сумму цифр в числе

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear();
+int Stepen(string message ){
+
+}
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+int stepen = 
+
+
---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Clear();
+int Prompt(string message){
+    System.Console.Write(message);//Выводим предложение ко входу
+    string readInput=System.Console.ReadLine();
+    int result = int.Parse(readInput);//Приводим к числу
+    return result;//возвращаем результат
+}
+int Summa(int number){
+    int result = 0;
+    if (number < 0 ){
+        number = number*(-1);
+        result = result + number%10;  // 9012 %10 =2 %100=12 /10= 901
+        number = number/10;
+
+    }
+    while (number > 0){
+        result = result + number%10;  // 9012 %10 =2 %100=12 /10= 901
+        number = number/10;
+
+    }
+    return result;
+}
+int number = Prompt("Введите число: ");
+Console.WriteLine($"{number} = {Summa(number)}");
